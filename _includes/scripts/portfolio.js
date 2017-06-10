fa286486f38ff0f2cdead9135a6509bdde288cbf
MingLee02 <ming.lee@hotmail.com>
2017-06-10 20:50:27 +0100

    dry code

--- _includes/scripts/portfolio.js
+++ _includes/scripts/portfolio.js
@@ -1,10 +1,9 @@
 const showAll = function() {
-    document.getElementById('work').style.display = '';
-    document.getElementById('personal').style.display = '';
+	const ids = document.querySelectorAll('*[id]:not(#portfolio)');
+    ids.forEach( id => { id.style.display = '' })
 }
 
 const filterWork = function (clickedId) {
 	const ids = document.querySelectorAll('*[id]:not(#portfolio)');
-
-	ids.forEach( id => { if (id.id === clickedId) id.style.display = '' else id.style.display = 'none'})
+	ids.forEach( id => { id.id === clickedId ? id.style.display = '' : id.style.display = 'none' })
 }
